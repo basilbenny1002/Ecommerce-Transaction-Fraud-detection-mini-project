2fd882eb506680e185ee5434932bbe0b302b8cd7
basilbenny1002 <basilbenny1002@gmail.com>
2025-05-11 21:49:26 +0530

    Add requirements.txt and fix some bugs in train.py

--- train.py
+++ train.py
@@ -7,7 +7,10 @@ import numpy as np
 df = pd.read_csv('onlinefraud.csv')
 #print(df.head())
 #Remove unnecessary columns
-df = df.drop('step')
+df = pd.get_dummies(df, columns=['type'], drop_first=True)
+df = df.drop(columns=['nameOrig', 'nameDest'])
+
+df = df.drop(columns=['step'],)
 
 # Separate features (X) and target variable (y)
 X = df.drop('isFraud', axis=1)
@@ -20,10 +23,12 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Initialize XGBoost classifier
 xgb_model = xgb.XGBClassifier(objective='binary:logistic',  # For binary classification
                              use_label_encoder=False, # Suppress the warning about deprecated parameter
-                             eval_metric='logloss', tree_method='gpu_hist')  # Specify evaluation metric for binary classification
+                             eval_metric='logloss', tree_method='gpu_hist',  enable_categorical=True)  # Specify evaluation metric for binary classification
 
 # Train the model
 xgb_model.fit(X_train, y_train, eval_set=[(X_test, y_test)], verbose=False)
+
+#Saving the mdoel in .pkl format
 with open('model.pkl', 'wb') as f:
     pickle.dump(xgb_model, f)
 
@@ -36,6 +41,7 @@ precision = precision_score(y_test, y_pred)
 recall = recall_score(y_test, y_pred)
 f1 = f1_score(y_test, y_pred)
 
+#Printing out the accuracy values
 print(f"Accuracy: {accuracy}")
 print(f"Precision: {precision}")
 print(f"Recall: {recall}")
