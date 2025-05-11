0a443735fe1d09a2764fb8e9d69affad2fb03264
basilbenny1002 <basilbenny1002@gmail.com>
2025-05-11 20:24:48 +0530

    Initial commit

---
+++ App.py
@@ -0,0 +1,67 @@
+import flet as ft
+
+# def main(page: ft.Page):
+#     page.title = "Flet counter example"
+#     page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+#     txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+
+#     def minus_click(e):
+#         txt_number.value = str(int(txt_number.value) - 1)
+#         page.update()
+
+#     def plus_click(e):
+#         txt_number.value = str(int(txt_number.value) + 1)
+#         page.update()
+
+#     page.add(
+#         ft.Row(
+#             [
+#                 ft.IconButton(ft.Icons.REMOVE, on_click=minus_click),
+#                 txt_number,
+#                 ft.IconButton(ft.Icons.ADD, on_click=plus_click),
+#             ],
+#             alignment=ft.MainAxisAlignment.CENTER,
+#         )
+#     )
+
+# ft.app(main)
+#TODO SAMPLE GUI
+import flet as ft
+
+class App:
+    def __init__(self):
+        self.page = None
+        self.txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+
+    def main(self, page: ft.Page):
+        self.page = page
+        self.page.title = "Windows Media Search"
+        self.page.window_width = 500
+        self.page.window_height = 400
+        self.page.bgcolor = '#a3b9ff'
+        self.page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+        # Create buttons
+        minus_btn = ft.ElevatedButton("-", on_click=self.minus_click)
+        plus_btn = ft.ElevatedButton("+", on_click=self.plus_click)
+
+        # Add all to page
+        self.page.add(
+            ft.Row(
+                [minus_btn, self.txt_number, plus_btn],
+                alignment=ft.MainAxisAlignment.CENTER
+            )
+        )
+
+    def minus_click(self, e):
+        self.txt_number.value = str(int(self.txt_number.value) - 1)
+        self.page.update()
+
+    def plus_click(self, e):
+        self.txt_number.value = str(int(self.txt_number.value) + 1)
+        self.page.update()
+
+if __name__ == '__main__':
+    app = App()
+    ft.app(target=app.main)
---
+++ train.py
@@ -0,0 +1,42 @@
+import xgboost as xgb
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import pandas as pd
+import numpy as np
+df = pd.read_csv('onlinefraud.csv')
+#print(df.head())
+#Remove unnecessary columns
+df = df.drop('step')
+
+# Separate features (X) and target variable (y)
+X = df.drop('isFraud', axis=1)
+y = df['isFraud']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+# Initialize XGBoost classifier
+xgb_model = xgb.XGBClassifier(objective='binary:logistic',  # For binary classification
+                             use_label_encoder=False, # Suppress the warning about deprecated parameter
+                             eval_metric='logloss', tree_method='gpu_hist')  # Specify evaluation metric for binary classification
+
+# Train the model
+xgb_model.fit(X_train, y_train, eval_set=[(X_test, y_test)], verbose=False)
+with open('model.pkl', 'wb') as f:
+    pickle.dump(xgb_model, f)
+
+# Make predictions
+y_pred = xgb_model.predict(X_test)
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+print(f"Accuracy: {accuracy}")
+print(f"Precision: {precision}")
+print(f"Recall: {recall}")
+print(f"F1-score: {f1}")
