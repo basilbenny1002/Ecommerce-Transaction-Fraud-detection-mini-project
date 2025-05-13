651129600972a8481b30503be699c9b01f773c9e
basilbenny1002 <basilbenny1002@gmail.com>
2025-05-14 02:16:44 +0530

    Refactor user management functions to include API key generation and enhance user data retrieval

--- Database_functions.py
+++ Database_functions.py
@@ -1,17 +1,45 @@
 import sqlite3
+import random
+import string
+
+def generate_api_key(length):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for _ in range(length))
+
+
+
 
 def add_new_user(mail: str, password: str, user_id: str):
     try:
+        API_KEY = generate_api_key(16)
         conn = sqlite3.connect('users.db') 
         cursor = conn.cursor()
-        cursor.execute("CREATE TABLE IF NOT EXIST USERS (user_id VARCHAR(255), mail VARCHAR(255), password VARCHAR(255))") 
-        cursor.execute(f"INSERT INTO USERS VALUES ('{user_id}', '{mail}', '{password}')")
+        cursor.execute("CREATE TABLE IF NOT EXISTS USERS (user_id VARCHAR(255) primary key, mail VARCHAR(255), password VARCHAR(255), API_KEY VARCHAR(255))") 
+        cursor.execute(f"INSERT INTO USERS VALUES ('{user_id}', '{mail}', '{password}', '{API_KEY}')")
         conn.commit()
         conn.close()
+    except sqlite3.IntegrityError:
+        print("Error: User with this user_id already exists.")
+        return "User already exists"
     except Exception as e:
         print(f"Some error occurred {e}")
+        return "Failed"
     else:
         print("data inserted successfully")
+        return "Success"
+
+def get_user_data(user_id: str):
+    try:
+        conn = sqlite3.connect('users.db') 
+        cursor = conn.cursor()
+        cursor.execute(f"SELECT * FROM USERS WHERE user_id = '{user_id}'")
+    except Exception as e:
+        print(f"Some error occurred {e}")
+        return "Failed"
+    else:
+        data = cursor.fetchall()
+
+
 
 # cursor.execute('''INSERT INTO STUDENT VALUES ('Raju', '7th', 'A')''') 
 # cursor.execute('''INSERT INTO STUDENT VALUES ('Shyam', '8th', 'B')''') 
